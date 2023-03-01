514d02bffe66db43499a3f448d2f8852e9fbfdab
enitchals <ellen.nitchals@gmail.com>
2023-02-28 21:05:20 -0800

    don't mutate arrays... and other changes

--- src/App.js
+++ src/App.js
@@ -14,32 +14,36 @@ function App() {
 
   const getReading = async() => {
     const reading = await getTarotReading(card, orientation)
-    console.log(reading)
     setReading(reading);
-    // setReading("a reading")
+    // setTimeout(() => {
+    //   setReading("You are in the process of opening yourself up to personal growth and expansion. The energy around you is guiding you towards taking some bold and important steps for your future. Stay mindful of your surroundings, trust your intuition, and be open to new opportunities along the way. The hard work you are doing now will pay off in the long run and benefit you greatly.")
+    // }, 3000)
   }
 
   const draw = (card) => {
     setCard(card);
-    const randomOrientation = Math.floor(Math.random() * 2); // get the current time and check digit for user-induced randomness
-    setOrientation(randomOrientation);
+      const randomOrientation = Math.floor(Math.random() * 2);
+      setOrientation(randomOrientation);
     getReading();
   }
 
   const shuffledCards = () => {
     const array = [];
+    const cardsToShuffle = [].concat(cards)
     for (let i = 0; i< 78; i++){
-      let random = Math.floor(Math.random() * cards.length);
-      array.push(cards.splice(random, 1)[0]);
+      let random = Math.floor(Math.random() * cardsToShuffle.length);
+      array.push(cardsToShuffle.splice(random, 1)[0]);
     }
     return array;
   }
 
+  const shuffled = shuffledCards();
+
   return (
-    <div className="App">
+    <div className="app">
       {card !== ''
-        ? <div><Reading reading={reading}/></div>
-        : <Cards cards={shuffledCards()} setCard={draw}/>
+        ? <Reading reading={reading} card={card} orientation={orientation}/>
+        : <Cards cards={shuffled} setCard={draw}/>
       }
     </div>
   );
